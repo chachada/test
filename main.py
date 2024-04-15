04a17e81973521c5a4754a3fe6f4ace772a7a60b
hera317088@gmail.com <HERA317088@gmail.com>
2024-04-15 14:05:41 +0900

    feat return

--- main.py
+++ main.py
@@ -70,9 +70,6 @@ class CompletionExecutor:
             'Accept': 'text/event-stream'
         }
 
-        responses = []
-        done_received = False  # "DONE" 응답이 수신되었는지 여부를 나타내는 플래그
-
         with requests.post(self._host + '/testapp/v1/chat-completions/HCX-003',
                            headers=headers, json=completion_request, stream=True) as r:
             for line in r.iter_lines():
@@ -81,18 +78,8 @@ class CompletionExecutor:
                     if response.startswith('data:{"message":{"role":"assistant","content":'):
                         response_text = response.split('"content":"')[-1]
                         response_text = response_text.split('"}')[0]
-                        response_text = response_text.replace('\\n', ' ')  # 개행 문자 삭제
-                        responses.append(response_text)
-                    elif response.strip() == 'data:{"message":{"role":"assistant","content":"DONE"}}':
-                        done_received = True  # "DONE" 응답 수신
-
-                    if done_received:
-                        break  # "DONE" 응답을 받은 후에는 더 이상 응답을 처리하지 않음
-
-        # 스트림으로 받은 응답 데이터를 하나의 문자열로 합침
-        full_response = ''.join(responses)
-        return full_response
-
+                        response_text = response_text.replace('\\n', '\n')
+                        st.chat_message("assistant").write(response_text)
 
 
 if __name__ == '__main__':
@@ -117,13 +104,13 @@ if __name__ == '__main__':
         # 결과를 출력한다.
         for result in query_result.matches:
             id = result.id
-            text = result.metadata['text']  # 문서의 원본 텍스트
+            text = result.metadata['text'] #문서의 원본 텍스트
             # title = result.metadata['title'] #문서의 제목
-            score = result.score  # 문서의 유사도
-            print(id, score)
+            score = result.score #문서의 유사도
+            print(id,score)
             print("\n")
             print(text)
-            print('=' * 10)
+            print('='*10)
 
         preset_text = [{"role": "system",
                         "content": " - 다음 문서에만 기반하여 질문에 대답합니다. - 문서에서 알수 없는 내용인 경우 '자세한 사항은 반드시 견본주택 및 고객센터로 확인해 주시기 바랍니다. (안내사항의 오류가 있을 시는 관계법령이 우선합니다.) ' 라고 합니다."},
@@ -149,13 +136,14 @@ if __name__ == '__main__':
 
         completion_executor = CompletionExecutor(
             host='https://clovastudio.stream.ntruss.com',
-            api_key=API_KEY,
-            api_key_primary_val=API_KEY_PRIMARY_VAL,
-            request_id=REQUEST_ID
+            api_key= API_KEY ,
+            api_key_primary_val= API_KEY_PRIMARY_VAL ,
+            request_id= REQUEST_ID
         )
 
         response = completion_executor.execute(request_data)
 
+
         msg = response
-        st.markdown(msg)
+        st.write(msg)
         st.chat_message("assistant").write(response)
