886aa939f9a77cf2c56baf43226427d1150ddf1d
hera317088@gmail.com <HERA317088@gmail.com>
2024-04-17 22:00:52 +0900

    return

--- main.py
+++ main.py
@@ -92,7 +92,7 @@ class CompletionExecutor:
             'Accept': 'text/event-stream'
         }
 
-        assistant_response = ""  # Assistant 응답을 저장할 변수
+        responses = []
         done_received = False  # "DONE" 응답이 수신되었는지 여부를 나타내는 플래그
 
         with requests.post(self._host + '/testapp/v1/chat-completions/HCX-003',
@@ -104,12 +104,14 @@ class CompletionExecutor:
                         response_text = response.split('"content":"')[-1]
                         response_text = response_text.split('"}')[0]
                         response_text = response_text.replace('\\n', '\n')
-                        assistant_response = response_text  # Assistant 응답 업데이트
-                    elif response.strip() == 'data:{"message":{"role":"assistant","content":"DONE"}}':
-                        done_received = True  # "DONE" 응답 수신
-                        break  # "DONE" 응답을 받은 후에는 더 이상 응답을 처리하지 않음
+                        responses.append(response_text)
+                        if response.strip() == 'data:{"message":{"role":"assistant","content":"DONE"}}':
+                            done_received = True  # "DONE" 응답 수신
+                            break  # "DONE" 응답을 받은 후에는 더 이상 응답을 처리하지 않음
 
-        return assistant_response
+        # 스트림으로 받은 응답 데이터를 하나의 문자열로 합침
+        full_response = ''.join(responses)
+        return full_response
 
 
 if __name__ == '__main__':
@@ -173,4 +175,5 @@ if __name__ == '__main__':
 
         response = completion_executor.execute(request_data)
 
-        st.chat_message("assistant").write(response)
+        msg = response
+        st.chat_message("assistant").write(msg)
