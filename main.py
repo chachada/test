968973fb674f0c346eccd8a0a8878a300fa9617b
hera317088@gmail.com <HERA317088@gmail.com>
2024-04-17 23:24:25 +0900

    ed

--- main.py
+++ main.py
@@ -102,8 +102,10 @@ class CompletionExecutor(BaseCallbackHandler):
                 if line:
                     response = line.decode("utf-8")
                     # print(response)
-                    if response.startswith('event:token'):
+                    if response.startswith('event:token'):                  
                         print("ok")
+                    else:
+                        print("fail")
                         if response.startswith('data:{"message":{"role":"assistant","content":'):
                             response_text = response.split('"content":"')[-1]
                             response_text = response_text.split('"}')[0]
